192d1434963c35ea1d31f2f4ad34006645b7e366
Kirk Dirk <jackalex@ya.ru>
2022-12-14 22:09:17 +0500

    initial commit + add Products.java

---
+++ Products.java
@@ -0,0 +1,43 @@
+public class Products {
+    /**
+     * Параметры "продукта"
+     */
+    private String name;
+    private double price;
+
+    /**
+     * Ввод "Продукта" с параметрами
+     * @param name // Наименование "продукта"
+     * @param price // Цена "продукта"
+     */
+    public Products(String name, double price){
+        this.setName(name);
+        this.setPrice(price);
+    }
+    /**
+     * Устанавливаем цену "продукта"
+     * @param price2
+     */
+    private void setPrice(double price2) {
+        this.price = price2;
+    }
+    /**
+     * Устанавливаем наименование "продукта"
+     * @param name2
+     */
+    private void setName(String name2) {
+        this.name = name2;
+    }
+    /**
+     * Возвращаем наименование "продукта"
+     * @return
+     */
+    public String getName() {return name;}
+    /**
+     * Возвращаем цену продукта
+     * @return
+     */
+    public double getPrice() {return price;}
+
+
+}
